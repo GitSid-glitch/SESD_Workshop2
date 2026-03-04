9d7080d9630aa491e87ef873b68b5ef559fa25dc
Siddharth <siddharth.shukla@adypu.edu.in>
2026-03-04 21:07:41 +0530

    Adding subtract command

---
+++ commands/subtract_command.ts
@@ -0,0 +1,32 @@
+class SubtractCommand {
+
+    program
+
+    constructor(program){
+        this.program = program
+    }
+
+    register(){
+
+        this.program
+        .command("subtract <a> <b>")
+        .description("Subtract two numbers")
+        .action((a, b) => {
+
+            this.subtractNumbers(a, b)
+
+        })
+
+    }
+
+    subtractNumbers(a, b){
+
+        const result = Number(a) - Number(b)
+
+        console.log("Result:", result)
+
+    }
+
+}
+
+module.exports = SubtractCommand
