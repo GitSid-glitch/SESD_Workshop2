e5e6073997cc77cc5bff17f5539d2048da127a39
Siddharth <siddharth.shukla@adypu.edu.in>
2026-03-04 21:21:49 +0530

    Adding the joke api and command

---
+++ commands/joke_command.ts
@@ -0,0 +1,35 @@
+const JokeService = require("../services/joke_service")
+
+class JokeCommand {
+
+    program
+    jokeService
+
+    constructor(program){
+
+        this.program = program
+        this.jokeService = new JokeService()
+
+    }
+
+    register(){
+
+        this.program
+        .command("joke")
+        .description("Get a random joke")
+        .action(async ()=>{
+
+            const joke = await this.jokeService.getJoke()
+
+            console.log(joke.setup)
+            console.log(joke.punchline)
+
+        })
+
+    }
+
+}
+
+module.exports = JokeCommand
+
+export {}
---
+++ services/joke_service.ts
@@ -0,0 +1,19 @@
+const axios = require("axios")
+
+class JokeService {
+
+    async getJoke(){
+
+        const res = await axios.get(
+            "https://official-joke-api.appspot.com/jokes/random"
+        )
+
+        return res.data
+
+    }
+
+}
+
+module.exports = JokeService
+
+export {}
