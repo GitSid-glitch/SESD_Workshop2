f0981ba64073ceb485e868a9516a5e5ff46de17b
Siddharth <siddharth.shukla@adypu.edu.in>
2026-03-04 21:08:09 +0530

    Adding the multiply command

---
+++ commands/multiply_command.ts
@@ -0,0 +1,32 @@
+class MultiplyCommand {
+
+    program
+
+    constructor(program){
+        this.program = program
+    }
+
+    register(){
+
+        this.program
+        .command("multiply <a> <b>")
+        .description("Multiply two numbers")
+        .action((a, b) => {
+
+            this.multiplyNumbers(a, b)
+
+        })
+
+    }
+
+    multiplyNumbers(a, b){
+
+        const result = Number(a) * Number(b)
+
+        console.log("Result:", result)
+
+    }
+
+}
+
+module.exports = MultiplyCommand
