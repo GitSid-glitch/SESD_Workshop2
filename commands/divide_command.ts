373349393b4639149c51bcb70f5894322c75c9ce
Siddharth <siddharth.shukla@adypu.edu.in>
2026-03-04 21:08:31 +0530

    Adding the divide command

---
+++ commands/divide_command.ts
@@ -0,0 +1,40 @@
+class DivideCommand {
+
+    program
+
+    constructor(program){
+        this.program = program
+    }
+
+    register(){
+
+        this.program
+        .command("divide <a> <b>")
+        .description("Divide two numbers")
+        .action((a, b) => {
+
+            this.divideNumbers(a, b)
+
+        })
+
+    }
+
+    divideNumbers(a, b){
+
+        const numA = Number(a)
+        const numB = Number(b)
+
+        if(numB === 0){
+            console.log("Cannot divide by zero")
+            return
+        }
+
+        const result = numA / numB
+
+        console.log("Result:", result)
+
+    }
+
+}
+
+module.exports = DivideCommand
